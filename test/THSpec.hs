f39efd2c23db0a031dd9dd3344ce6eb20d21f0a5
gareth <gstokes@superit.ai>
2026-06-08 20:12:57 +1000

    test(th): generated entity round-trips through the UoW

--- test/THSpec.hs
+++ test/THSpec.hs
@@ -3,10 +3,10 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE OverloadedLabels #-}
 
 module THSpec (tests) where
 
+import qualified Data.ByteString
 import Data.Text (Text)
 import Manifest (Key (..), Entity (..), withSession, add, get)
 import Manifest.Core.Meta (ColumnMeta (..), SqlType (..), tmTable, tmColumns)
@@ -24,6 +24,13 @@ $(mkEntity "Widget" "widgets"
     , field "size" [t| Maybe Int |]
     ])
 
+widgetsDDL :: Data.ByteString.ByteString
+widgetsDDL =
+  "CREATE TABLE widgets \
+  \( widget_id   BIGSERIAL PRIMARY KEY \
+  \, widget_name TEXT NOT NULL \
+  \, widget_size BIGINT )"
+
 tests :: [Test]
 tests = group "TH"
   [ test "mkEntity generates correct table metadata" $ do
@@ -38,4 +45,14 @@ tests = group "TH"
   , test "mkEntity wires primKey to the PrimaryKey field" $
       assertEqual "primKey selects widget_id" 7
         (primKey (Widget { widgetId = 7, widgetName = "x", widgetSize = Nothing } :: Widget))
+  , test "generated entity round-trips: add (eager, RETURNING) then get decodes from the DB" $
+      withEmptyDb $ \pool -> do
+        withConnection pool (\c -> execText c widgetsDDL [])
+        w0 <- withSession pool $
+          add (Widget { widgetId = 0, widgetName = "gizmo", widgetSize = Just 7 } :: Widget)
+        assertBool "add filled the serial PK" (widgetId w0 > 0)
+        got <- withSession pool $ get @Widget (Key (widgetId w0))
+        assertEqual "name decodes" (Just "gizmo")       (fmap widgetName got)
+        assertEqual "size decodes" (Just (Just 7))      (fmap widgetSize got)
+        assertEqual "pk decodes"   (Just (widgetId w0)) (fmap widgetId got)
   ]
